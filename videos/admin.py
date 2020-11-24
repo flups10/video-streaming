ce0eda84e58c5f143f5f0bf820e5ad317e6f62e3
flups10 <je.tieleman@gmail.com>
2020-11-24 14:40:57 +0000

    Created videos app and Model structure for it

---
+++ videos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Serie, Episode
+
+# Register your models here.
+admin.site.register(Serie)
+admin.site.register(Episode)
---
+++ videos/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Serie(models.Model):
+    name = models.CharField(max_length=99)
+    friendly_name = models.CharField(max_length=99, null=True, blank=True)
+    genre = models.CharField(max_length=99)
+    episodes_count = models.IntegerField()
+    description = models.TextField()
+    image_url = models.URLField(max_length=999, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Episode(models.Model):
+    name = models.CharField(max_length=99)
+    series = models.ForeignKey('Serie', null=True, blank=True, on_delete=models.SET_NULL)
+    video_url = models.URLField(max_length=999)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
